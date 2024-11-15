b64d28c14f0188401a8981009d7276deeca5eba8
Iliya Belyak <iliyabel2003@outlook.com>
2024-11-14 21:22:23 -0800

    Circles shrink when they are not within. Circles have a max growth size.

--- canvas.js
+++ canvas.js
@@ -67,6 +67,18 @@ var mouse = {
     y: undefined
 }
 
+var maxRadius = 44;
+var minRadius = 3;
+
+
+var colorArray = [
+    '#3F3F37',
+    '#D6D6B1',
+    '#494331',
+    '#878472',
+    '#DE541E',    
+];
+
 
 window.addEventListener('mousemove', 
     function(event) {
@@ -85,7 +97,6 @@ function Circle(x, y, dx, dy, radius) {
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = 'blue';
         c.fill();
         c.stroke();
     }
@@ -106,7 +117,13 @@ function Circle(x, y, dx, dy, radius) {
         // interactivity
         if (mouse.x - this.x < 50 && mouse.x - this.x > -50
             && mouse.y - this.y < 50  &&  mouse.y - this.y > -50) {
-            this.radius += 1;
+
+            if (this.radius < maxRadius) {
+                this.radius += 1;   
+            }
+
+        } else if (this.radius > minRadius) {
+            this.radius -= 1;
         }
 
 
@@ -118,15 +135,15 @@ function Circle(x, y, dx, dy, radius) {
 
 var circleArray = [];
 
-for (var i = 0; i < 100; i++) {
+for (var i = 0; i < 120; i++) {
 
     var radius = 30
 
     var x = Math.random() * (window.innerWidth - radius * 2) + radius;
     var y = Math.random() * (window.innerHeight - radius * 2) + radius;
 
-    var dx = (Math.random() - 0.5) * 1;
-    var dy = (Math.random() - 0.5 )* 10;
+    var dx = (Math.random() - 0.5) * 2;
+    var dy = (Math.random() - 0.5 )* 4;
 
     circleArray.push(new Circle(x, y, dx, dy, radius));
 }
